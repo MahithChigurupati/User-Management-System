a675cefa94f180ca2bf64e21edcf54c484cac0f4
Mahith Chigurupati <saimahith.chigurupati@gmail.com>
2023-11-12 14:38:04 -0500

    get user & update functionality

---
+++ User Management System/EditUserView.swift
@@ -0,0 +1,18 @@
+//
+//  EditUserView.swift
+//  User Management System
+//
+//  Created by Mahith ‎ on 11/12/23.
+//
+
+import SwiftUI
+
+struct EditUserView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditUserView()
+}
