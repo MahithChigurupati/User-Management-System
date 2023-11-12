f26f05c92eac6941f5a19a264723d176639283d7
Mahith Chigurupati <saimahith.chigurupati@gmail.com>
2023-11-12 00:42:42 -0500

    Add user functinality

---
+++ User Management System/AddUserView.swift
@@ -0,0 +1,18 @@
+//
+//  AddUserView.swift
+//  User Management System
+//
+//  Created by Mahith ‎ on 11/11/23.
+//
+
+import SwiftUI
+
+struct AddUserView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddUserView()
+}
