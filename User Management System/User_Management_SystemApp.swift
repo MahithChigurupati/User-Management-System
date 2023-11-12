af2b83f2c120806dc76b0c1f157c6ec3b27809ec
Mahith Chigurupati <saimahith.chigurupati@gmail.com>
2023-11-11 21:57:36 -0500

    Initial Commit

---
+++ User Management System/User_Management_SystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  User_Management_SystemApp.swift
+//  User Management System
+//
+//  Created by Mahith ‎ on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct User_Management_SystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
