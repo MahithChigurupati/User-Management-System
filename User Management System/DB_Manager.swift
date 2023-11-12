90c28ff9a3fb88d090b0b610436b9ebc84e800a0
Mahith Chigurupati <saimahith.chigurupati@gmail.com>
2023-11-11 23:38:32 -0500

    SQlite DB installation and user model setup

---
+++ User Management System/DB_Manager.swift
@@ -0,0 +1,8 @@
+//
+//  DB_Manager.swift
+//  User Management System
+//
+//  Created by Mahith ‎ on 11/11/23.
+//
+
+import Foundation
---
+++ User Management System/UserModel.swift
@@ -0,0 +1,8 @@
+//
+//  UserModel.swift
+//  User Management System
+//
+//  Created by Mahith ‎ on 11/11/23.
+//
+
+import Foundation
